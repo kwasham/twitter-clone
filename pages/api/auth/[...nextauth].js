45988251cef372d2d4991d506872624b602108b7
kwasham <kwashamdev@gmail.com>
2022-04-21 10:42:59 -0500

    next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,15 +13,17 @@ export default NextAuth({
 
   callbacks: {
     // ...add custom callbacks here
-    async session({session, token}) {
-        
-        // @ts-ignore
-        session.user.tag = session.user.name.split(" ").join("").toLowerCase()
+    async session({ session, token }) {
+      // @ts-ignore
+      session.user.tag = session.user.name.split(' ').join('').toLowerCase()
 
-        // @ts-ignore
-        session.user.uid = token.sub
-        return session
+      // @ts-ignore
+      session.user.uid = token.sub
+      return session
+    },
   },
-},
-secret: process.env.JWT_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
 })
